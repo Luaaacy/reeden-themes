141e4d33e19f0dd5956f40a67b6f11190146738c
Luaaacy <l18746073174@gmail.com>
2026-09-08 22:15:32 +0800

    Add files via upload

---
+++ paidThemes.js
@@ -0,0 +1,59 @@
+const paidThemesData = [
+    {
+        isPaid: true, // 关键标识：设置为 true 代表付费主题
+        search: "命运线 韩系 浪漫 七夕 限定 付费 简约",
+        coverImg: "./images/命运线/一版预览.png",
+        date: "Aug 20, 2026",
+        title: "命运线",
+        subtitle: "Bound by an invisible thread，we were destined to collide.Exclusive Premium Theme.",
+        tags: ["简约", "韩系", "浪漫", "七夕", "甜蜜", "恋爱感", "付费限定"],
+        detailImgs: "./images/命运线/一版预览.png,./images/命运线/二版预览.png",
+        startup: "2个",
+        carousel: "4个",
+        cover: "4个",
+        homeBg: "1个",
+        readBg: "2个",
+        highlight: "对话",
+        intro: "七夕做的一个韩系简约浪漫恋爱风格的主题<br><br>非常适合看点小甜文<br><br>其实是一个从相识到订婚的过程<br><br>两个版本<br><br>3个页面均独立设计，采用不同构图和素材搭配，减少重复感",
+        xhsLink: "https://xhslink.cn/o/2VhR0ynV9Ox" // 填写对应小红书笔记链接
+    },
+    {
+        isPaid: true, // 关键标识：设置为 true 代表付费主题
+        search: "小生活 简约 多色 九色 基础 百搭",
+        coverImg: "./images/小生活/预览首图.jpg",
+        date: "Aug 31, 2026",
+        title: "小生活",
+        subtitle: "Collecting quiet moments of everyday magic one ordinary day at a time.",
+        tags: ["简约", "多色", "九色", "基础", "百搭"],
+        detailImgs: "./images/小生活/预览首图.jpg",
+        startup: "有",
+        carousel: "1个",
+        cover: "12个",
+        homeBg: "1个纯色",
+        readBg: "至少1个，个别颜色多个背景",
+        highlight: "对话",
+        intro: "一个非常百搭的简约基础款主题<br><br>删删改改做了6天<br><br>做出来发现也非常适合小字党<br><br>有12个日历封面，尽量减少重复<br><br>9种配色分别用了对应色系图片素材，不是简单的替换色值<br><br>现在已经用小彩色看美了哈哈~<br><br>☀️日间8色：<br><br>白色，灰咖，浅咖，淡粉，淡蓝，淡黄，淡紫，薄荷绿<br><br>🌙夜间黑色：<br><br>黑色绑在夜间模式，所有颜色都有夜间（黑色） ",
+        xhsLink: "https://xhslink.cn/o/4YKqU02opHx", // 填写对应小红书笔记链接
+    },
+    {
+        isPaid: true, // 关键标识：设置为 true 代表付费主题
+        search: "小生活 简约 动态 春夏 明媚 治愈 基础 百搭",
+        coverImg: "./images/小生活动态版/动态预览1.png",
+        date: "Sep 5, 2026",
+        title: "小生活动态版",
+        subtitle: "返图福利款",
+        tags: ["简约", "动态", "春夏", "明媚", "治愈"],
+        detailImgs: "./images/小生活动态版/动态预览1.png,./images/小生活动态版/动态预览2.png",
+        startup: "有",
+        carousel: "1个动态轮播图",
+        cover: "16个，8个动态封面，8个静态封面",
+        homeBg: "无",
+        readBg: "2个",
+        highlight: "双色对话",
+        intro: "一个简约动态主题<br><br>春夏治愈生活感<br><br>终于学会做动图啦！<br><br>还比较简陋<br><br>所以只作为返图福利款",
+        xhsLink: "https://xhslink.cn/o/9E6oxgrMohI", // 填写对应小红书笔记链接
+        noticeTitle: "✦ 返图福利款 ✦", // 提示框小标题（可选，不写默认显示 BENEFIT THEME）
+        noticeText: "此主题为<strong>购买小生活九色全套</strong>返图福利款！<br><br>请前往小红书该笔记下<strong>点赞+关注并带图评论</strong>，私信凭截图领取。" // 领取要求（每次可以自由修改，支持 <strong>加粗</strong>）
+    }
+
+];

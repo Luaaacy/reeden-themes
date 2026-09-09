b284a1e83d099e5fb601ef5241f1af0694c6321d
Luaaacy <l18746073174@gmail.com>
2026-09-09 19:30:17 +0800

    Update themes.js

--- themes.js
+++ themes.js
@@ -120,7 +120,7 @@ const themesData = [
         readBg: "1个",
         highlight: "对话",
         intro: "一个韩系Kpop主题<br><br>仿的是QQ音乐浅色轻盈彩胶播放器样式<br><br>看到的时候就觉得非常适合做成主题<br><br>但我失败了，好像毁了<br><br>就这样吧<br><br>图片找了很久，都是QQ音乐的专辑封面图<br><br>按照合适的颜色选的图，和个人喜好、排名无关<br><br>大家可以二改自己喜欢的图片<br><br>有小生活紫色版的也可以搭配紫色日历轮播图，非常适配！",
-        xhsLink: "https://xhslink.cn/o/9sOgIbg0ac3",
+        xhsLink: "https://xhslink.cn/o/7kgDhK5qp0E",
         diskBase64: "aHR0cHM6Ly9wYW4uYmFpZHUuY29tL3MvMVdnZUJ0djNEeTVrbmdLM3RsRmJGZWc="
     },
     
